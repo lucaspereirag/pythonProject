d1517ec5d27e263a834176a51d181d25ce5819b0
Lucas Pereira Galves <lucaspereiragalves@gmail.com>
2020-12-28 14:55:57 -0300

    ex028 updated

--- ex028.py
+++ ex028.py
@@ -1,10 +1,13 @@
 import random
+from time import sleep
 
 numint = random.randint(0, 5) #Computador emite um número aleatório
 #print(numint)
 print('-=-' * 20)
 numUser = int(input('Digite um número entre 0 e 5: ')) #Usuário digita um número aleatório
 print('-=-' * 20)
+print('Processando os dados...')
+sleep(1)
 if numUser == numint: #Faz a Validação entre Computador x Usuário
     print('Seu número {} é igual ao {} escolhido pelo computador. \nParabéns!'
           .format(numUser, numint))
