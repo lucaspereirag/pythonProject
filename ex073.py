7c368ca2d393dfae3efc5fb6aaa549cf6c1547c8
Lucas Pereira Galves <lucaspereiragalves@gmail.com>
2021-01-09 01:03:44 -0300

    Fatiamento de uma Tupla com Times

---
+++ ex073.py
@@ -0,0 +1,12 @@
+times = ('São Paulo', 'Internacional', 'Atletico-MG', 'Flamegno', 'Gremio', 'Palmeiras',
+         'Fluminense', 'Santos', 'Corinthians', 'Athletico-PR', 'Ceara', 'Atletico-GO',
+         'Bragantino', 'Sport', 'Fortaleza', 'Vasco', 'Bahia', 'Goias', 'Botafogo', 'Coritiba')
+
+
+print(f'Os 5 primeiros times no momento: {times[:5]}')
+print('-x' * 40)
+print(f'Os últimos 4 colocados no momento: {times[-4:]}')
+print('-x' * 40)
+print(f'A lista em ordem alfabética: {sorted(times)}')
+print('-x' * 40)
+  print(f'A posição que o São Paulo está: {times.index("São Paulo") + 1}º')
